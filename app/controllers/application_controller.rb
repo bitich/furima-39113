035626c918c9ec41fa4ad31fbc6984455b59ea56
bitich <bitich777@yahoo.co.jp>
2023-07-06 22:52:34 +0900

    devise導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 
   class ApplicationController < ActionController::Base
     before_action :basic_auth
-  
+    before_action :configure_permitted_parameters, if: :devise_controller?
   
     private
   
@@ -10,5 +10,10 @@
         username == 'admin' && password == '2222'
       end
     end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+    
   end
 
