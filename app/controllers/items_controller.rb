7c2519dc3a15b8e20b99caf348138126d3acb474
bitich <bitich777@yahoo.co.jp>
2023-07-10 21:39:46 +0900

    出品するをクリックすると画面遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,18 @@
 class ItemsController < ApplicationController
-
+   
 
   def index
 
   end
 
+  def new
+     unless user_signed_in?
+        redirect_to action: :index
+     end
+  end
+    
 
 
-
+  
   
 end
