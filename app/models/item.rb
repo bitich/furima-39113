54dd8425aa9cd9aaebea8a4da1cfac30bb184c9f
bitich <bitich777@yahoo.co.jp>
2023-07-12 19:44:17 +0900

    エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,15 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :price, presence: true ,numericality: {greater_than:299,less_than:999999}
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :postage
-  belongs_to :prefecture
-  belongs_to :shipment
+  belongs_to :category 
+  belongs_to :condition 
+  belongs_to :postage 
+  belongs_to :prefecture 
+  belongs_to :shipment 
+  validates :category_id , numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id , numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id , numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipment_id , numericality: { other_than: 1 , message: "can't be blank"}
+
 end
