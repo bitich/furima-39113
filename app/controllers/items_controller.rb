686be685df494537edf46afcef80579e3f12077c
bitich <bitich777@yahoo.co.jp>
2023-07-12 16:10:57 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,29 @@ class ItemsController < ApplicationController
      unless user_signed_in?
         redirect_to action: :index
      end
+
+     @item = Item.new
+
   end
+
+  def create
+     @item = Item.new(item_params)
+
+    if @item.save
+        redirect_to root_path
+    else
+        render :new
+    end
+
+  end
+
+
+  private
     
+    def item_params
+     params.require(:item).permit(:name,:explanation,:image,:category_id,:price).merge(user_id: current_user.id)
+      end
 
-
-  
-  
 end
+  
+
---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipment
+end
