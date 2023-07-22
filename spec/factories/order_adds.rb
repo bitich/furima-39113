272b474e433b8ca888ab400b844f0d5e22b571db
bitich <bitich777@yahoo.co.jp>
2023-07-22 15:14:55 +0900

    単体コードの実装

--- spec/factories/order_adds.rb
+++ spec/factories/order_adds.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
-  factory :item_add do
-    post {111-1111}
+  factory :order_add do
+    post {"111-1111"}
+    item_id {1}
     prefecture_id {2}
     city {"大野城市"}
     address {"1-1-2"}
     tell {"09073855911"}
     token {"tok_abcdefghijk00000000000000000"}
+    
+    association :user 
+
 end
+end
+
